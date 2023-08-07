e12238ef0d12ae37d70bedf926a52e1804e1036c
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-08-07 14:14:49 +0100

    REMOVED: uneeded sanity call in SectionProjects component

--- app/components/SectionProjects.tsx
+++ app/components/SectionProjects.tsx
@@ -1,22 +1,9 @@
 import ProjectCard from "./ProjectCard"
-import { groq } from "next-sanity"
-import { client } from "../lib/sanity"
-import { Projects } from "../lib/interface"
-
-async function getData() {
-  const query = groq`*[ _type == "projects"]`
-  const data = await client.fetch(query)
-  return data as Projects[]
-}
 
 export default async function SectionProjects() {
-  const data = (await getData()) as Projects[]
-
   return (
     <>
-      {data.map((project) => (
-        <h2 key={project._id}>{project.sectionHeading}</h2>
-      ))}
+      <h2>Projects()</h2>
       <section>
         <ProjectCard />
       </section>
