d53febf32ea3752352c60066dd1ebd6e61b7e946
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-08-12 10:59:30 +0100

    ADDED: SectionContact component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Intro from "./components/Intro"
 import SectionAbout from "./components/SectionAbout"
 import SectionProjects from "./components/SectionProjects"
 import SectionTools from "./components/SectionTools"
+import SectionContact from "./components/SectionContact"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionProjects />
       <SectionAbout />
       <SectionTools />
+      <SectionContact />
     </>
   )
 }
