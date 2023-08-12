883ba7d6093fed08df258d33209185d7a6111c2a
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-08-12 10:31:53 +0100

    UPDATED: sanity call for header, intro & navbar components

--- app/lib/getSanityData.ts
+++ app/lib/getSanityData.ts
@@ -1,8 +1,14 @@
 import { groq } from "next-sanity"
 import { client } from "./sanity"
 
-export async function getData() {
+export async function getSiteIntro() {
   const query = groq`*[_type == "siteIntro"]`
+  const data = await client.fetch(query)
+  return data
+}
+
+export async function getNavData() {
+  const query = groq`*[_type == "navbarLinks"] | order(order asc)`
   const data = await client.fetch(query)
   return data
 }
