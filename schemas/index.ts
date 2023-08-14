b823999091847f58d2cb564ffe1ad1aa9064522e
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-08-14 08:11:51 +0100

    ADDED: metadata schema UPDATED: interfaces with PortableTextBlock type

--- schemas/index.ts
+++ schemas/index.ts
@@ -4,6 +4,7 @@ import projects from "./projects"
 import about from "./about"
 import tools from "./tools"
 import blog from "./blog"
+import footer from "./footer"
 import metadata from "./metadata"
 
-export const schemaTypes = [intro, navbarLinks, projects, about, tools, blog, metadata]
+export const schemaTypes = [intro, navbarLinks, projects, about, tools, blog, footer, metadata]
