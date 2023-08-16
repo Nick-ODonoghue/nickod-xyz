3f794ceb6a82c3e9eb29258e4f43f40c4fac517d
Nick-ODonoghue <NicholasGODonoghue@gmail.com>
2023-08-16 13:14:42 +0100

    FIXED: robots.ts sitemap url failure

--- app/robots.ts
+++ app/robots.ts
@@ -3,13 +3,13 @@ import { getSiteMetadata } from "./lib/getSanityData"
 import { Metadata } from "./lib/interface"
 
 export default async function robots(): Promise<MetadataRoute.Robots> {
-  const url = (await getSiteMetadata()) as Metadata
+  const meta = (await getSiteMetadata()) as Metadata
 
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: `${url}sitemap.xml`,
+    sitemap: `${meta.siteUrl}sitemap.xml`,
   }
 }
